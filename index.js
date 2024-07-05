d00f1e51122dde14f930274f982edb8cd8844975
xu_yanfeng <xu_yanfeng@126.com>
2024-07-05 11:59:24 +0800

    add some api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 function tip() {
-    console.log(`Don't call me, I'm just an empty shell`);
+  console.log(`Don't call me, I'm just an empty shell`);
 }
 module.exports = {
-    ensureDirSync: tip,
-    emptyDirSync: tip,
-    copySync: tip,
-    copyFileSync: tip,
+  ensureDirSync: tip,
+  emptyDirSync: tip,
+  copySync: tip,
+  copyFileSync: tip,
+  existsSync: tip,
+  writeFileSync: tip,
 }
